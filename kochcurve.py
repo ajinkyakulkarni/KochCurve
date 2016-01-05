52f3382a787bb44a42ad0622ab4515e200bc4c95
MaximilianoGG <mxgdvg@gmail.com>
2016-01-05 22:08:02 +0100

    modified kochcurve.py

--- kochcurve.py
+++ kochcurve.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import Tkinter as tk
+#  kochcurve.py
+#  
+#  2016 Maximiliano G. G.
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+from sys import exit
+from math import cos, sin, radians
+
+try:
+    import Tkinter as tk # Python < 3
+except ImportError:
+    try:
+        import tkinter as tk
+    except ImportError:
+        sys.exit("No tkinter module installed")
 
 WIDTH=600
 HEIGHT=600
+
 window = tk.Tk()
 window.title("Koch Curve")
-canvas = tk.Canvas(window, width=600, height=600, bg='white')
-canvas.pack()
 
-from math import cos, sin, radians, sqrt
+canvas = tk.Canvas(window, width=WIDTH, height=HEIGHT, bg='white')
+canvas.pack(side=tk.TOP)
 
 class Point:
-    # a list with two items
     def __init__(self, xcoord=0, ycoord=0):
         self.point = [xcoord, ycoord]
     
@@ -24,40 +51,49 @@ class Point:
         self.point[index]= value
     
     def __str__(self):
+        ''' Return a string describing the point '''
         return "Point: " + str(self.point)
         
 class Vector:
-    # Position vector.
+    ''' Position vector
+    
+    Although you can use a point as a position vector, they are conceptually different.
+    '''
     def __init__(self, point):
-        # point is the tip, [0,0] the tail
+        # point is the tip (and Point [0,0] the tail)
         self.point = point
     
     def __getitem__(self, index):
         return self.point[index]
     
     def __str__(self):
-        return "Vector: <" + str(self.point[0]) +', ' + str(self.point[1]) + '>'
+        ''' Return a string describing the vector '''
+        return 'Vector: <' + str(self.point[0]) +', ' + str(self.point[1]) + '>'
 
 class Line:
     def __init__(self, start, end, level=0):
-        ''' create a line from Point or position vector start to Point or p-v end and depth level level'''
+        ''' Create a line from start to end
+        
+        start & end are Points or Position vectors while level specifies the
+        depth level or step at which the line belongs.
+        '''
         self.start = start
         self.end = end
-        self.level = level
+        self.level = level # we can access this value directly or defining a getter/setter
         
     def draw(self, color='blue'):
-        ''' draw the line on the canvas'''
+        ''' Draw the line on the canvas '''
         canvas.create_line(self.start[0], self.start[1], self.end[0], self.end[1],
             fill=color, tags='line')
         
     def __str__(self):
-        ''' return a string describing the line state'''
+        ''' Return a string describing the line '''
         return "Line: " + str(self.start) + '-' + str(self.end) + ' Level ' + str(self.level)
 
 def get_sublines(line):
     ''' Return a list of 4 sublines of line line
     
-    Create 4 new lines using subdivision points of line
+    Create 4 new lines using subdivision points of line.
     '''
     points = get_subdivisions(line.start, line.end)
     lines = []
@@ -73,35 +109,27 @@ def get_position_vector(A, B):
     return Vector(Point(B[0]-A[0], B[1]-A[1]))
 
 def rotate(axis, point, grades):
-    # TODO eliminar calculos y accesos repetidos
     new_point = Point()
-    new_point[0]=axis[0] + (point[0]-axis[0])*cos(radians(grades)) - (point[1]-axis[1])*sin(radians(grades))
-    new_point[1]=axis[1] + (point[0]-axis[0])*sin(radians(grades)) + (point[1]-axis[1])*cos(radians(grades))
+    r_grades = radians(grades)
+    new_point[0]=axis[0] + (point[0]-axis[0])*cos(r_grades) - (point[1]-axis[1])*sin(r_grades)
+    new_point[1]=axis[1] + (point[0]-axis[0])*sin(r_grades) + (point[1]-axis[1])*cos(r_grades)
     return new_point
-
-def drawLine(start, end, color='blue'):
-    # A & B are position vectors or points
-    canvas.create_line(start[0], start[1], end[0], end[1], fill=color, tags='line')
     
 def get_subdivisions(tail, tip):
-    # tail & tip are position vectors or just points defining a vector that represemt a line
-    ''' return a list with position vectors to subdivisions '''
+    ''' return a list with position vectors to subdivisions
+    
+    tail(A) & tip(E) are position vectors or just points defining a vector that represent a line
+         /C\
+    A__B/   \D__E
+    '''
     v = get_position_vector(tail, tip)
     B = Vector(Point(v[0]/3.0 + tail[0], v[1]/3.0 + tail[1]))
     D = Vector(Point(v[0]*2/3 + tail[0], v[1]*2/3 + tail[1]))
     C = Vector(rotate(B, D, -60))
-    pointlist = []
-    pointlist.append(tail)
-    pointlist.append(B)
-    pointlist.append(C)
-    pointlist.append(D)
-    pointlist.append(tip)    
-    return pointlist
-
-def must_draw(line):
-    # check if line is at final level and therefore should be drawn.
-    # class Line has an attribute level which defines its own level
+    return [tail,B,C,D,tip]
     
+def must_draw(line):
+    ''' Check if line is at final level and therefore should be drawn. '''
     level = int(spinbox.get()) # level better global updated every time buttons clicked
     # the problem of changing the spinbox while drawing
     
@@ -111,24 +139,35 @@ def kochCurve(line):
     ''' Generate and draw a Koch curve.
     
     Call this function three times for each line of an equilateral triangle
-    and get a snowflake. This is the main backtracking algorythm.
+    and get a snowflake. This is the main backtracking algorythm. Cool!
     '''
     if must_draw(line):
         line.draw()
-        #canvas.update()
+        #canvas.update() # uncomment this line to see how it is created step by step
     else:
         for subline in get_sublines(line):
             kochCurve(subline)
 
 def drawKoch(event):
-    # if event from button Curve draw a curve
-    canvas.delete('line') # clear the canvas
+    canvas.delete('line') # clean the canvas
+    
     if event.widget == buttonCurve:
+        # clicked buttonCurve
         start = Point(0,HEIGHT*2/3)
         end = Point(WIDTH,HEIGHT*2/3)
-        kochCurve(Line(start, end)) # vuelta-atrás
-    # if event from button snowflake draw a snowflake
-
+        kochCurve(Line(start, end)) # backtracking
+    elif event.widget == buttonSnowflake:
+        # clicked buttonSnowflake
+        # call three times kochCurve for each line of an equilateral triangle
+        point1 = Point(100,175)
+        point2 = Point(WIDTH-100,175)
+        point3 = rotate(point1, point2, 60)
+        kochCurve(Line(point1, point2))
+        kochCurve(Line(point2, point3))
+        kochCurve(Line(point3, point1))
+    
+# Buttons and Spinbox
+#
 # button for create a koch curve
 buttonCurve = tk.Button(window, text="Koch Curve")
 buttonCurve.bind('<Button-1>', drawKoch)
@@ -148,18 +187,6 @@ spinbox = tk.Spinbox(window, from_=0, to=10, increment=1, width=2)
 spinbox.pack(side=tk.LEFT)    
     
 def test():
-    p1 = Point(100,350)
-    print p1
-    p2 = Point(500,150)
-    l = Line(p1, p2)
-    print l
-    #for subline in get_sublines(l): print subline
-    vectors = get_subdivisions(Vector(p1), Vector(p2))
-    for v in vectors: print v
-    drawLine(vectors[0], vectors[1])
-    drawLine(vectors[1], vectors[2])
-    drawLine(vectors[2], vectors[3])
-    drawLine(vectors[3], vectors[4])
     return 0
 
 if __name__ == '__main__':
